f84ed318e170c3c38075b9e3de9089f9937739f5
sgareilforcity <sebastien.gareil@forcity.io>
2017-01-19 17:15:53 +0100

    fix IT hello bob

--- tests/integration/test_integration_service.py
+++ tests/integration/test_integration_service.py
@@ -2,8 +2,8 @@ from module_to_test.service import ClassToTest
 
 def test_list_local():
     service = ClassToTest( )
-    assert service.hello("bob") == "Hello bob"
+    assert service.hello("bob") == "hello bob"
 
 def test_list_remote():
     service = ClassToTest()
-    assert service.hello("eponge") == "Hello, Foobar from pull_push!"
+    assert service.hello("eponge") == "hello, Foobar from pull_push!"
