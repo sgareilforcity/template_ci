b511efe5341e07c4c22060a4338dc37eaf470244
sgareilforcity <sebastien.gareil@forcity.io>
2017-01-23 17:46:28 +0100

    rename integration test

--- module_to_test/service.py
+++ module_to_test/service.py
@@ -1,4 +1,7 @@
 class ClassToTest(object):
 
     def hello(self, name=str):
-        return "hello %s" % name
+        if (name is not None or name != ""):
+            return "hello %s" % name
+        else:
+            return "bye bye !"
