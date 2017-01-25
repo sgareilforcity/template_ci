f968b322cdd81d290c2b9788f5f64cf2a63c38bf
sgareilforcity <sebastien.gareil@forcity.io>
2017-01-25 11:54:40 +0100

    adding fixme and todo

--- module_to_test/service.py
+++ module_to_test/service.py
@@ -1,7 +1,21 @@
 class ClassToTest(object):
 
-    def hello(self, name=str):
+    def run(self, name=str):
+        #IDEA factorize the return with the method hello.
         if (name is not None or name != ""):
             return "hello %s" % name
         else:
             return "bye bye !"
+
+    @classmethod
+    def hello(self, name=str):
+        #FIXME implements a performant algorythme.
+        if (name is not None or name != ""):
+            return "hello %s" % name
+        else:
+            return "bye bye !"
+
+    @property
+    def why_the_life(self):
+        #TODO implements the solution of the world.
+        return "42"
