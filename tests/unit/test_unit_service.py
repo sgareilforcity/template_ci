f5ba76f7c603440bd1ecebff6dee9ab0d96ea5df
sgareilforcity <sebastien.gareil@forcity.io>
2017-02-02 17:54:35 +0100

    fix IT hello bob

--- tests/unit/test_unit_service.py
+++ tests/unit/test_unit_service.py
@@ -2,7 +2,7 @@ from module_to_test.service import ClassToTest
 
 def test_hello_bob():
     service = ClassToTest( )
-    assert service.hello("bob") == "Hello bob"
+    assert service.hello("bob") == "hello bob"
 
 def test_always_true():
     assert True
